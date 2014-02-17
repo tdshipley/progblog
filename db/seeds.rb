211abb14cf746ab8b16a5d4f8563a31296fd9e29
Thomas Shipley <thomas.shipley@googlemail.com>
2014-02-17 22:04:38 +0000

    Added seed data. And support for html tags in posts also added more scss styling around paragraphs.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+posts = []
+
+posts << Post.new(:title => "Working as a developer", :body => "<p>Is awesome</p>".html_safe)
+
+posts << Post.new(:title => "Ruby", :body => "<p>Is a consise language which allows you to be
+expressive. You can write your code in a simplistic way using concepts such as poetry mode;
+which does away with keystrokes infavour of more readable english like syntax.</p>".html_safe)
+
+posts << Post.new(:title => "Ruby On Rails", :body => "<p>Ruby on rails is a web framework
+which is simple and powerful.</p> <p>If you need a framework which will get out of your way
+and let you focus on developing your web application then this is the web framework for you.
+It encorages convention over configuration allowing you to forget about annoying configuration
+details</p> <p>Using Rails you can increase your productivity by being consise in code thanks
+to the power of ruby and also automation of key developer tasks such as testing - rails has
+powerful frameworks baked in. But you can even use 3rd party ones easily with gems</p>".html_safe)
+
+posts.each do |post|
+  post.save!
+end
+
+railspost_comments = posts[2].comments
+
+railspost_comments
+        .create(:name => "Thomas", :body => "The speed that you can create an app is really nice for proof of concept apps.")
+        .save!
+railspost_comments
+        .create(:name => "James", :body => "I like it. I would like to know a little more about what is going on under the hood
+        however. A lot of rails at the moment just feel a little too much like magic!")
+        .save!
